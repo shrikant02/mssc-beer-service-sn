232f9bcfa35ff305d99b4340321e91bee701cfc2
shrikant02 <shrikantnavghare2001@gmail.com>
2023-08-24 23:54:10 +0530

    Adding Jackson config to BeetDto

--- src/main/java/com/shrikant/web/model/BeerDto.java
+++ src/main/java/com/shrikant/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package com.shrikant.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
@@ -41,6 +42,7 @@ public class BeerDto {
     @NotNull
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
